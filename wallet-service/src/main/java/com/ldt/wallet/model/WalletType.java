5865b4f7da7b2d29a0e8fb2c899113132bf83474
LeDanhTien <danhtien2k4@gmail.com>
2026-02-09 16:38:51 +0700

    feat: add model for wallet service

--- wallet-service/src/main/java/com/ldt/wallet/model/WalletType.java
+++ wallet-service/src/main/java/com/ldt/wallet/model/WalletType.java
@@ -3,14 +3,14 @@ package com.ldt.wallet.model;
 import lombok.Getter;
 
 @Getter
-public enum WalletStatus {
+public enum WalletType {
     ADMIN_WALLET("ADMIN_WALLET", "Ví quản trị viên"),
     USER_WALLET("USER_WALLET", "Ví người dùng");
 
     private final String code;
     private final String description;
 
-    WalletStatus(String code, String description) {
+    WalletType(String code, String description) {
         this.code = code;
         this.description = description;
     }
