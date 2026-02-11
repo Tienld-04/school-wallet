db6065f1d78d51cb055a89b955c58861fd34a665
LeDanhTien <danhtien2k4@gmail.com>
2026-02-11 16:29:51 +0700

    chore: Temporarily suspend payment service

--- payment-service/src/main/java/com/ldt/payment/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/ldt/payment/PaymentServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaymentServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PaymentServiceApplication.class, args);
+        System.out.println("Temporarily suspend payment service.");
     }
 
 }
