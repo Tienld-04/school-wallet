763f9d0a3a6aba5dc6d630ef0668dc614da30f30
LeDanhTien <danhtien2k4@gmail.com>
2026-03-12 22:46:35 +0700

    fix: algorithm jwt

--- api-gateway/src/main/java/com/ldt/gateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/ldt/gateway/util/JwtUtil.java
@@ -1,7 +1,27 @@
 package com.ldt.gateway.util;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.*;
 
-@Component
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
 public class JwtUtil {
+    @Value("${jwt.secret-key}")
+    protected String SECRET_KEY;
+     @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        SecretKeySpec key = new SecretKeySpec(
+                SECRET_KEY.getBytes(StandardCharsets.UTF_8),
+                "HmacSHA512"  
+        );
+        return NimbusReactiveJwtDecoder
+                .withSecretKey(key)
+                .macAlgorithm(MacAlgorithm.HS512)
+                .build();
+    }
 }
