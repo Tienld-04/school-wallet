48ee8c1e57e5419e19744b58fc13d48115c7182c
LeDanhTien <danhtien2k4@gmail.com>
2026-03-23 14:58:33 +0700

    feat: add filter global for api wallet service

--- wallet-service/src/main/java/com/ldt/wallet/controller/WalletController.java
+++ wallet-service/src/main/java/com/ldt/wallet/controller/WalletController.java
@@ -1,12 +1,28 @@
 package com.ldt.wallet.controller;
 
+import com.ldt.wallet.context.UserContext;
+import com.ldt.wallet.dto.response.BalanceResponse;
+import com.ldt.wallet.service.WalletService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
-@RequestMapping("/wallets")
+@RequestMapping("/api/wallets")
 @RequiredArgsConstructor
 public class WalletController {
 
+    private final WalletService walletService;
+
+    @GetMapping("/my-balance")
+    public ResponseEntity<BalanceResponse> getBalanceByUserId() {
+        String userId = UserContext.getUserId();
+        BalanceResponse balance = walletService.getBalanceByUserId(userId);
+        return ResponseEntity.ok(balance);
+    }
 }
