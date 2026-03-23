c6bf7dc69a154f98cfaf3e13bc4437dcd0819b82
LeDanhTien <danhtien2k4@gmail.com>
2026-03-23 23:07:48 +0700

    chore: update wallet type

--- wallet-service/src/main/java/com/ldt/wallet/model/WalletType.java
+++ wallet-service/src/main/java/com/ldt/wallet/model/WalletType.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum WalletType {
     ADMIN_WALLET("ADMIN_WALLET", "Ví quản trị viên"),
-    USER_WALLET("USER_WALLET", "Ví người dùng");
+    USER_WALLET("USER_WALLET", "Ví người dùng"),
+    MERCHANT_WALLET("MERCHANT_WALLET", "Ví nhà cung cấp dịch vụ");
 
     private final String code;
     private final String description;
