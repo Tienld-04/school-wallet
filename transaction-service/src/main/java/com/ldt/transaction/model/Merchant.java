9aaabac65e073076d679add9215e31063f329759
LeDanhTien <danhtien2k4@gmail.com>
2026-03-24 19:18:45 +0700

    chore: update merchant model

--- transaction-service/src/main/java/com/ldt/transaction/model/Merchant.java
+++ transaction-service/src/main/java/com/ldt/transaction/model/Merchant.java
@@ -2,12 +2,18 @@ package com.ldt.transaction.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "merchants")
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -25,22 +31,26 @@ public class Merchant {
     @Column(name = "type", nullable = false, length = 50)
     @Enumerated(EnumType.STRING)
     private MerchantType type;
+    // wallet id
+    @Column(name = "wallet_id", nullable = false)
+    private UUID walletId;
 
     @Column(name = "active")
     private Boolean active = true;
 
+    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @PrePersist
-    protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
-    }
-    @PreUpdate
-    protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
+    @CreatedBy
+    @Column(name = "create_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "update_by")
+    private String updatedBy;
 }
