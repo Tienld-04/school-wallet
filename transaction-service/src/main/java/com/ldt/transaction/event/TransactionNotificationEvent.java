1c466566d0526c0129844d6a73ff8d9568c2065d
LeDanhTien <danhtien2k4@gmail.com>
2026-03-27 13:54:48 +0700

    feat: add message transaction with activemq

---
+++ transaction-service/src/main/java/com/ldt/transaction/event/TransactionNotificationEvent.java
@@ -0,0 +1,26 @@
+package com.ldt.transaction.event;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransactionNotificationEvent implements Serializable {
+    private UUID transactionId;
+    private UUID fromUserId;
+    private String fromFullName;
+    private String fromPhone;
+    private UUID toUserId;
+    private String toFullName;
+    private String toPhone;
+    private BigDecimal amount;
+    private String description;
+    private String transactionType;
+    private String status;
+}
--- transaction-service/src/main/java/com/ldt/transaction/service/TransactionService.java
+++ transaction-service/src/main/java/com/ldt/transaction/service/TransactionService.java
@@ -2,6 +2,7 @@ package com.ldt.transaction.service;
 
 import com.ldt.transaction.dto.TransactionResponse;
 import com.ldt.transaction.dto.TransferRequest;
+import com.ldt.transaction.event.TransactionNotificationEvent;
 import com.ldt.transaction.dto.transfer.WalletTransferRequest;
 import com.ldt.transaction.dto.user.InternalVerifyPinRequest;
 import com.ldt.transaction.dto.user.UserInternalResponse;
@@ -11,12 +12,15 @@ import com.ldt.transaction.mapper.TransactionMapper;
 import com.ldt.transaction.model.Transaction;
 import com.ldt.transaction.model.TransactionStatus;
 import com.ldt.transaction.model.TransactionType;
+import com.ldt.transaction.producer.TransactionEventProducer;
 import com.ldt.transaction.repository.TransactionRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronization;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
@@ -27,6 +31,7 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final RestTemplate restTemplate;
     private final TransactionMapper transactionMapper;
+    private final TransactionEventProducer notificationProducer;
     @Value("${service.wallet-service.url}")
     private String walletServiceUrl;
 
@@ -84,12 +89,7 @@ public class TransactionService {
         transaction.setToPhone(toUser.getPhone());
         transaction.setToFullName(toUser.getFullName());
         transaction.setAmount(transferRequest.getAmount());
-        transaction.setDescription(
-                "Từ: " + fromUser.getFullName() + " (" + fromUser.getPhone() + ")"
-                        + " → Đến: " + toUser.getFullName() + " (" + toUser.getPhone() + ")"
-                        + " | Số tiền: " + transferRequest.getAmount()
-                        + " | Nội dung: " + transferRequest.getDescription()
-        );
+        transaction.setDescription(transferRequest.getDescription());
         transaction.setTransactionType(TransactionType.TRANSFER);
         transaction.setStatus(TransactionStatus.PENDING);
         transaction = transactionRepository.save(transaction);
@@ -108,34 +108,32 @@ public class TransactionService {
         } catch (Exception ex) {
             transaction.setStatus(TransactionStatus.FAILED);
             transaction = transactionRepository.save(transaction);
-//            throw new RuntimeException("Chuyển tiền thất bại: " + ex.getMessage());
             return transactionMapper.toTransactionResponse(transaction);
         }
         transaction = transactionRepository.save(transaction);
-        return transactionMapper.toTransactionResponse(transaction);
 
-        // call wallet-service
-//        WalletTransferRequest walletReq = new WalletTransferRequest();
-//        walletReq.setFromUserId(fromUser.getUserId());
-//        walletReq.setToUserId(toUser.getUserId());
-//        walletReq.setAmount(transferRequest.getAmount());
-//        restTemplate.postForEntity(
-//                walletServiceUrl + "/internal/wallets/transfer",
-//                walletReq,
-//                Void.class
-//        );
-//        Transaction transaction = new Transaction();
-//        transaction.setRequestId(transferRequest.getRequestId());
-//        transaction.setFromUserId(fromUser.getUserId());
-//        transaction.setToUserId(toUser.getUserId());
-//        transaction.setAmount(transferRequest.getAmount());
-//        transaction.setDescription("Ví ID: " + fromUser.getUserId()
-//                + " -" + transferRequest.getAmount() + " -> "
-//                + "Ví ID: " + toUser.getUserId() + " +" + transferRequest.getAmount()
-//                + ", Nội dung: " + transferRequest.getDescription());
-//        transaction.setTransactionType(TransactionType.TRANSFER);
-//        transaction.setStatus(TransactionStatus.SUCCESS);
-//        transaction = transactionRepository.save(transaction);
-//        return transactionMapper.toTransactionResponse(transaction);
+        // Event transfer -> ActiveMq
+        TransactionNotificationEvent event = TransactionNotificationEvent.builder()
+                .transactionId(transaction.getTransactionId())
+                .fromUserId(transaction.getFromUserId())
+                .fromFullName(transaction.getFromFullName())
+                .fromPhone(transaction.getFromPhone())
+                .toUserId(transaction.getToUserId())
+                .toFullName(transaction.getToFullName())
+                .toPhone(transaction.getToPhone())
+                .amount(transaction.getAmount())
+                .description(transaction.getDescription())
+                .transactionType(transaction.getTransactionType().name())
+                .status(transaction.getStatus().name())
+                .build();
+
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                notificationProducer.sendNotification(event);
+            }
+        });
+
+        return transactionMapper.toTransactionResponse(transaction);
     }
 }
