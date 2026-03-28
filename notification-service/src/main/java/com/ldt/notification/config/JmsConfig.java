43ab6c9f9f85d524af3d2f67a26d2cb5207f827f
LeDanhTien <danhtien2k4@gmail.com>
2026-03-28 23:30:04 +0700

    fix: update Jms config

--- notification-service/src/main/java/com/ldt/notification/config/JmsConfig.java
+++ notification-service/src/main/java/com/ldt/notification/config/JmsConfig.java
@@ -2,11 +2,13 @@ package com.ldt.notification.config;
 
 import jakarta.jms.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
 
 @Configuration
 @EnableJms
@@ -14,13 +16,28 @@ import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 public class JmsConfig {
 
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(@Qualifier("jmsConnectionFactory") ConnectionFactory connectionFactory) {
+    public SingleConnectionFactory connectionFactory(
+            @Value("${spring.activemq.broker-url}") String brokerUrl,
+            @Value("${spring.activemq.user}") String user,
+            @Value("${spring.activemq.password}") String password
+    ) {
+        ActiveMQConnectionFactory activeMQFactory = new ActiveMQConnectionFactory();
+        activeMQFactory.setBrokerURL(brokerUrl);
+        activeMQFactory.setUserName(user);
+        activeMQFactory.setPassword(password);
+
+        SingleConnectionFactory factory = new SingleConnectionFactory(activeMQFactory);
+        factory.setClientId("notification-service");
+        factory.setReconnectOnException(true);
+        return factory;
+    }
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(SingleConnectionFactory connectionFactory) {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setPubSubDomain(true);
-        // Durable subscription: giữ message khi consumer offline
         factory.setSubscriptionDurable(true);
-        factory.setClientId("notification-service");
         factory.setErrorHandler(t -> log.error("JMS listener error: {}", t.getMessage(), t));
         return factory;
     }
