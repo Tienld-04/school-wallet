b9573f2189197688424f080363faab0a06589db0
LeDanhTien <danhtien2k4@gmail.com>
2026-05-24 22:47:30 +0700

    feat: public check-phone

--- api-gateway/src/main/java/com/ldt/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ldt/gateway/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/register",
                                 "/api/v1/auth/forgot-password",
+                                "/api/v1/auth/check-phone",
                                 "/api/v1/otp/**",
                                 "/api/v1/transactions/topup/ipn",
                                 "/ws/**",
